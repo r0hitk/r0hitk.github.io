d0caad18072f68d544078a67a563a4bfe28890ff
Rohit Kumar <59209227+r0hitk@users.noreply.github.com>
2021-02-12 00:03:06 +0530

    reload bug

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link=>{
-    link.addEventListener("click",()=>{
-        document.body.classList.remove("nav-open");
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
 });
+
+window.onbeforeunload(() => window.scrollTo(0, 0));
